dc0becadae6e6f86fea86c7c8a6a9e2d92580c0c
Geethika <geethikachinni58@gmail.com>
2026-03-18 22:48:56 +0530

    Added Day_3 program

---
+++ Day_3.java
@@ -0,0 +1,19 @@
+public class Day_3 {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for(int i=0; i<=n; i++) 
+            sum += i;
+            System.out.println(sum);
+
+
+
+    // Method 2
+    int b = 4;
+    System.out.println(b*(b+1)/2);
+        
+    }
+}
+
+
